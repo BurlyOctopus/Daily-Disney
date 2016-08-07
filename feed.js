03e226468fde70caadca2bacd95fdf77d28496ce
BurlyOctopus <joseph.clarkson20@gmail.com>
2016-08-07 17:12:26 -0400

    Countdown/lazyload
    Added a countdown and kind of a lazy load but not happy with it.

--- feed.js
+++ feed.js
@@ -3,6 +3,22 @@ function randomUser(userArray){
   console.log(userArray);
   return userArray[Math.floor(Math.random()*userArray.length)];
 }
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
 function getPicture(the_group_id, your_div_class){
 		var apiKey = "2a8606045cc0cd8dcda41e5ffd2947f2"; // replace this with your API key
     var favoredUsers = ['49679809@N07','92238955@N06','91534967@N00'];
@@ -43,9 +59,28 @@ function getPicture(the_group_id, your_div_class){
 						function(response){
               console.log(response);
 							if(response.stat == 'ok'){
-								var the_url = response.sizes.size[9].source;
-                console.log(the_url);
-								$('.'+your_div_class).append('<img src="' + the_url + '" />');
+								var the_url_small = response.sizes.size[3].source;
+								var the_url_large = response.sizes.size[9].source;
+                function OnImageLoaded (img) {
+                  console.log(img);
+                  console.log(the_url_small);
+                  if(img.src == the_url_small){
+                    $('.'+your_div_class).append('<div class="smallImage" style="background-image: url(' + the_url_small + ');" />');
+                    $('.loader').addClass('finished');
+                    PreloadImage (the_url_large);
+                  } else{
+                    $('.'+your_div_class).append('<div class="largeImage" style="background-image: url(' + the_url_large + ');" />');
+                    $('.smallImage').addClass('fade');
+                  }
+            		}
+                function PreloadImage (src) {
+                  var img = new Image ();
+            			img.onload = function () {OnImageLoaded (this)};
+                  img.src = src;
+                }
+
+            		PreloadImage (the_url_small);
+								// $('.'+your_div_class).append('<div class="smallImage" style="background-image: url(' + the_url_small + ');" />');
 							}
 							else{
 								console.log(" The request to get the picture was not good :\ ")
@@ -61,4 +96,10 @@ function getPicture(the_group_id, your_div_class){
 		);
 	};
 
-getPicture('34741466@N00', 'imageWrapper');
+$(function() {
+  var endDate = getTimeRemaining('2016-9-9');
+  getPicture('34741466@N00', 'imageWrapper');
+
+  console.log(endDate.days);
+  $('.countDownDate .number').text(endDate.days);
+});
